10f4485ebaa9afbb66c7eaec0411449290dee750
Arattel <sizonov2000@gmail.com>
2018-10-23 15:15:03 +0300

    added a lot of stuff

--- string.c
+++ string.c
@@ -94,5 +94,57 @@ int my_str_copy(const my_str_t* from, my_str_t* to, int reserve){
     }
     to->data[to->size_m + 1] = '\0';
 }
+//! Очищає стрічку -- робить її порожньою. Складність має бути О(1).
+void my_str_clear(my_str_t* str){
+    my_str_create(str, str->capacity_m);
+}
 
+//! Вставити символ у стрічку в заданій позиції, змістивши решту символів праворуч.
+//! Якщо це неможливо, повертає -1, інакше 0.
+int my_str_insert_c(my_str_t* str, char c, size_t pos){
+    if(pos > str->size_m || pos >= str->capacity_m || (str->size_m + 1) > str->capacity_m){
+        return -1;
+    } else{
+        if(pos == str->size_m){
+            str->data[str->size_m] = c;
+            str->data[str->size_m + 1] = '\0';
+        } else{
+            for(int i = pos; i < str->size_m + 1; i++){
+                str->data[i + 1] = str->data[i];
+            }
+            str->data[pos] = c;
+            str->size_m++;
+        }
+        return 0;
+    }
+}
+//! Вставити стрічку в заданій позиції, змістивши решту символів праворуч.
+//! Якщо це неможливо, повертає -1, інакше 0.
+int my_str_insert(my_str_t* str, const my_str_t* from, size_t pos){
+    if(pos > str->size_m || pos >= str->capacity_m || (str->size_m + from->size_m) > str->capacity_m){
+        return -1;
+    } else{
+        if(pos == str->size_m){
+            for(int i  = pos; i < str->size_m + from->size_m; i++){
+                str->data[i] = from->data[i - str->size_m];
+            }
+            str->data[str->size_m + from->size_m] = '\0';
+        } else{
+            for(int i = pos; i < str->size_m + 1; i++){
+                str->data[i + from->size_m] = str->data[i];
+            }
+            for(int i  = pos; i < pos + from->size_m; i++){
+                str->data[i] = from->data[i - str->size_m];
+            }
+        }
+        str->size_m += from->size_m;
+        return 0;
+    }
+}
 
+void main(){
+    my_str_t test_str;
+    my_str_create(&test_str, 200);
+    my_str_pushback(&test_str, 'a');
+    print(&test_str);
+}
