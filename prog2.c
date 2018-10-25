44149f3e5b58b1cd8d869fb6c5e0b9bd7b211b87
nazariyb <bachynsky@ucu.edu.ua>
2018-10-25 15:40:31 +0300

    rm trash

---
+++ prog2.c
@@ -0,0 +1,11 @@
+//
+// Created by 3naza on 10/25/2018.
+//
+
+#include <stdio.h>
+#include "my_string.h"
+
+int main(int argc, char *argv[]) {
+    FILE* inp_file = fopen(argv[1], "r");
+    if (!inp_file) return EXIT_FAILURE;
+}
