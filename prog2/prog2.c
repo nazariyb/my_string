ca1a754871b9cd30e7a3a0a11b6ad494c9867fca
nazariyb <bachynsky.ucu.edu.ua>
2018-10-26 20:13:22 +0300

    it seems prog2 to be finished

--- prog2/prog2.c
+++ prog2/prog2.c
@@ -1,24 +1,56 @@
-//
-// Created by 3naza on 10/25/2018.
-//
 // команда для компіляції з консолі:
 // gcc prog2.c ../library/my_string.c ../library/my_string.h -o prog2test -I ../library
 
 #include <stdio.h>
+#include <ctype.h>
 #include "my_string.h"
 
+my_str_t get_out_file_name(char inp_file_name[]) {
+    
+    my_str_t out_file_name;
+    my_str_from_cstr(&out_file_name, inp_file_name, 104);
+    
+    my_str_t txt_str;
+    my_str_from_cstr(&txt_str, ".txt", 4);
+    
+    size_t out_pos = my_str_find(&out_file_name, &txt_str, 0);
+    my_str_insert_cstr(&out_file_name, "_out", out_pos);
+    
+    return out_file_name;
+}
+
 int main(int argc, char *argv[]) {
+
     FILE* inp_file = fopen(argv[1], "r");
     if (!inp_file) return EXIT_FAILURE;
+    
+    fseek(inp_file, 0, SEEK_END);
+    size_t file_size = ftell(inp_file);
+    fseek(inp_file, 0, SEEK_SET);
+
     my_str_t file_str;
-    my_str_create(&file_str, 1000);
-    char file_cstr[1000];
-    // char file_cstr2[1000];
-    // while (fscanf(inp_file, " %s", file_cstr)) {
-        // printf("%s\n", file_cstr);
-        // my_str_append_cstr(&file_str, file_cstr);
+    my_str_create(&file_str, file_size);
+
+
+    int c_int;
+    while ((c_int = getc(inp_file)) != EOF) {
+        char c = c_int;
+
+        if ispunct(c) continue;
+
+        if (c == ' ') {
+            if (my_str_getlast(&file_str) == ' ') continue;
+        }
+        my_str_pushback(&file_str, tolower(c));
         
-    // }
-    // int res2 = fscanf(inp_file, " %s", file_cstr2);
-    printf("%s\n", file_str.data);
+    }
+    
+    my_str_t out_file_name = get_out_file_name(argv[1]);
+
+    FILE* out_file = fopen(my_str_getdata(&out_file_name), "w");
+    fputs(my_str_getdata(&file_str), out_file);
+
+    fclose(inp_file);
+    fclose(out_file);
+    return EXIT_SUCCESS;
 }
