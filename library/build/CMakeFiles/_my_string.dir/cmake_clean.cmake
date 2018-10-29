0047179bd18cfd327e610b8213a9dd24f2588754
Arattel <sizonov2000@gmail.com>
2018-10-29 17:43:18 +0200

    Added prints, detected bugs

---
+++ library/build/CMakeFiles/_my_string.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/_my_string.dir/my_string.c.o"
+  "lib_my_string.pdb"
+  "lib_my_string.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/_my_string.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
